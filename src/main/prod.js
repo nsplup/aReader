0d24c088389ec3b835f1837da4c98252a65061a1
Luke Pan <asahi0749@gmail.com>
2021-05-31 15:09:29 +0800

    fix: 修复因为设置缩放倍率导致的界面样式错误

--- src/main/prod.js
+++ src/main/prod.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, webFrame } = require('electron')
 const { basic } = require('@/modules/basic')
 
 let win
@@ -18,6 +18,7 @@ function createWindow () {
   })
   
   win.loadFile('./build/dist/prod/index.html')
+  webFrame.setZoomFactor(1)
 }
 
 app.whenReady()
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -2,17 +2,11 @@ const path = require('path')
 const { merge } = require('webpack-merge')
 const conf = require('./webpack.config')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const TerserPlugin = require('terser-webpack-plugin')
 const outputPath = path.resolve(__dirname, 'build/dist/prod')
 const base = merge(conf, {
   output: {
     filename: '[name].js',
     path: outputPath,
-  },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin({ parallel: 4 })],
-    sideEffects: false,
   },
 })
 
